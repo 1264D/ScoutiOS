3d1fdb33f4f7b5985dc43a84d0d429fb09ed4a88
Seth <seth@Seths-iMac.local>
2016-10-07 12:56:52 -0700

    Initial Commit

---
+++ 1264Scout/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  1264Scout
+//
+//  Created by Seth on 10/7/16.
+//  Copyright © 2016 1264D. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
